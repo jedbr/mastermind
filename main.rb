02cca28f73f382949a4cf4822656dd6bb8cb3d3e
jedrzej bryll <jedrzej.bryll@gmail.com>
2016-10-12 21:23:07 +0200

    Remove unused reverse_each_with_index method

--- main.rb
+++ main.rb
@@ -1,12 +1,4 @@
 require_relative 'mastermind'
 
-class Array
-  def reverse_each_with_index &block
-    (0...size).reverse_each do |i|
-      block.call(self[i], i)
-    end
-  end
-end
-
 game = Mastermind.new
 game.play
